e9f1a724169ae6a8d41582e28e40a392ee2d6190
action <action@github.com>
2025-04-22 05:17:24 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,7 +1,10 @@
-//Tue Apr 22 2025 05:15:19 GMT+0000 (Coordinated Universal Time)
+//Tue Apr 22 2025 05:17:24 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-function hi() {
-  console.log("Hello World!");
-}
-hi();
+(function (_0x2065d4, _0x3f968c) {
+  (function () {})();
+  _0x2065d4.update = "2023年01月17日05:34:29更新";
+  _0x3f968c.info = "本站历时1年半研发的新版本V7初版，具有多态性加密，破解难度更高。";
+  _0x3f968c.feedback = "由于刚刚上线，有问题积极反馈，请联系QQ 84034666";
+  _0x3f968c.tips = "如遇不兼容，请先暂时用v6加密，并反馈给我们。";
+})(window, document);
