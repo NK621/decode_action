ce4146a8d4b9020a855a88cb969aad867ec22c22
action <action@github.com>
2025-04-22 05:21:02 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,10 +1,14 @@
-//Tue Apr 22 2025 05:17:24 GMT+0000 (Coordinated Universal Time)
+//Tue Apr 22 2025 05:21:02 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-(function (_0x2065d4, _0x3f968c) {
-  (function () {})();
-  _0x2065d4.update = "2023年01月17日05:34:29更新";
-  _0x3f968c.info = "本站历时1年半研发的新版本V7初版，具有多态性加密，破解难度更高。";
-  _0x3f968c.feedback = "由于刚刚上线，有问题积极反馈，请联系QQ 84034666";
-  _0x3f968c.tips = "如遇不兼容，请先暂时用v6加密，并反馈给我们。";
-})(window, document);
+(function () {
+  var _0xaba39c = "\u006A\u0073\u0068\u0061\u006D\u0061\u006E\u002E\u0063\u006F\u006D";
+  var _0x_0x3d6 = 2;
+  var _0xe5a73d = 2017;
+  _0x_0x3d6 = 7;
+  var _0xee27cc = function () {
+    return ")c(".split("").reverse().join("") + _0xe5a73d + "\u002D" + new Date()['\u0067\u0065\u0074\u0046\u0075\u006C\u006C\u0059\u0065\u0061\u0072']() + "\u002C" + _0xaba39c;
+  };
+  var _0xf_0xf02 = console['\u006C\u006F\u0067'];
+  _0xf_0xf02(_0xee27cc());
+})();
