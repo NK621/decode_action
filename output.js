6d751328ff92385013b804cb7973987c7261905c
action <action@github.com>
2025-05-15 15:54:43 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,187 +1,157 @@
-//Thu May 15 2025 13:27:41 GMT+0000 (Coordinated Universal Time)
+//Thu May 15 2025 15:54:42 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-var _0x2d_0xa9g = 12;
 let QQ = "";
-_0x2d_0xa9g = 5;
-var _0x893f = 12;
 let index = 0;
-_0x893f = 4;
 let AUTO_MSG = [];
-var _0x291fd = 9;
-let currentAutoIndex = 0;
-_0x291fd = 7;
-setData = function (qq, chatIndex, ...autoMessages) {
-  QQ = qq;
-  index = chatIndex;
-  AUTO_MSG = autoMessages;
+setData = function (_0x1f4e07, _0x43bf65, ..._0x406748) {
+  QQ = _0x1f4e07;
+  index = _0x43bf65;
+  AUTO_MSG = _0x406748;
 };
-setData("\u0031\u0032\u0033\u0034\u0035\u0036\u0037\u0038\u0039", 0, "\u4F60\u597D\u007C\u0068\u0065\u006C\u006C\u006F", "uoy teem ot ecin|\u4F60\u8BC6\u8BA4\u5174\u9AD8\u5F88".split("").reverse().join(""));
+setData("123456789", 0, "hello");
 MainFunc = () => {
-  function _0xa_0x4ed() {
-    function _0xf569a(message, buttonText = "\u006F\u006B") {
-      vue['\u0074\u006F\u0061\u0073\u0074\u0065\u0064']['\u0073\u0068\u006F\u0077'](message, {
-        "action": {
-          '\u0074\u0065\u0078\u0074': buttonText,
-          "onClick": (toast, cb) => {
-            cb['\u0067\u006F\u0041\u0077\u0061\u0079'](0);
+  function _0x1a8d9a() {
+    function _0x3c78a2(_0x569d39, _0xed9fe9 = "ok") {
+      vue.toasted.show(_0x569d39, {
+        action: {
+          text: _0xed9fe9,
+          onClick: (_0x35b2d0, _0x1419a8) => {
+            _0x1419a8.goAway(0);
           }
         }
       });
     }
-    function _0x6c36d(message) {
-      chatPage['\u006D\u0073\u0067\u0049\u006E\u0070\u0075\u0074'] = message;
-      chatPage['\u006D\u0073\u0067\u0053\u0065\u006E\u0064']();
+    function _0x43a392(_0x48cb1b) {
+      chatPage.msgInput = _0x48cb1b;
+      chatPage.msgSend();
     }
-    function _0x777cfd() {
-      chatPage['\u0073\u006F\u0063\u006B\u0065\u0074\u0073']['\u0065\u006D\u0069\u0074']("dmcsys".split("").reverse().join(""), {
-        '\u006D\u0073\u0067': "\u0065\u006E\u0064",
-        "countReceive": false,
-        "chatId": chatPage['\u0063\u0068\u0061\u0074\u0049\u0064'],
-        "countTalked": false
+    function _0x233162() {
+      chatPage.sockets.emit("syscmd", {
+        msg: "end",
+        countReceive: false,
+        chatId: chatPage.chatId,
+        countTalked: false
       });
-      chatPage['\u0063\u0068\u0061\u0074\u0053\u0074\u0061\u0074\u0065'] = "\u0053\u0045\u004C\u0046\u005F\u004C\u0045\u0046\u0054";
+      chatPage.chatState = "SELF_LEFT";
     }
-    function _0x4c87fd() {
-      chatPage['\u0073\u006F\u0063\u006B\u0065\u0074\u0073']['\u0065\u006D\u0069\u0074']("\u0073\u0079\u0073\u0063\u006D\u0064", {
-        '\u006D\u0073\u0067': "\u006E\u0065\u0077",
-        '\u0067\u0065\u006E\u0064\u0065\u0072': chatPage['\u0067\u0065\u006E\u0064\u0065\u0072'],
-        "vipCode": chatPage['\u0076\u0069\u0070\u0043\u006F\u0064\u0065'],
-        "age": store['\u0067\u0065\u0074']("\u0061\u0067\u0065\u004E\u0065\u0077"),
-        '\u0061\u0067\u0065\u0057\u0061\u006E\u0074': store['\u0067\u0065\u0074']("tnaWega".split("").reverse().join("")),
-        '\u0073\u0065\u0063\u0072\u0065\u0074\u0043\u006F\u0064\u0065': chatPage['\u0073\u0065\u0063\u0072\u0065\u0074\u0043\u006F\u0064\u0065'],
-        "province": chatPage['\u0070\u0072\u006F\u0076\u0069\u006E\u0063\u0065'],
-        "provinceWant": store['\u0067\u0065\u0074']("\u0070\u0072\u006F\u0076\u0069\u006E\u0063\u0065\u0057\u0061\u006E\u0074"),
-        '\u0073\u0061\u0076\u0065\u0064\u0049\u0064': chatPage['\u0075\u0073\u0072\u0049\u0064'],
-        '\u0069\u0073\u0053\u0068\u006F\u0077\u004C\u006F\u0063\u0061\u0074\u0069\u006F\u006E': false,
-        "fp": chatPage['\u0066\u0069\u006E\u0067\u0065\u0072\u0050\u0072\u0069\u006E\u0074\u0073'],
-        '\u0070\u0068\u006F\u006E\u0065\u004E\u0075\u006D\u0062\u0065\u0072': store['\u0067\u0065\u0074'](chatPage['\u0043\u006F\u006E\u0066\u0069\u0067']['\u0053\u0054\u004F\u0052\u0045\u005F\u0050\u0048\u004F\u004E\u0045\u005F\u004E\u0055\u004D\u0042\u0045\u0052']),
-        '\u0075\u0073\u0065\u0072\u0050\u0077\u0064\u0045\u006E\u0063\u0072\u0079\u0070\u0074\u0065\u0064': store['\u0067\u0065\u0074'](chatPage['\u0043\u006F\u006E\u0066\u0069\u0067']['\u0053\u0054\u004F\u0052\u0045\u005F\u0050\u0041\u0053\u0053\u0057\u004F\u0052\u0044\u005F\u0045\u004E\u0043\u0052\u0059\u0050\u0054\u0045\u0044'])
+    function _0x5607fd() {
+      chatPage.sockets.emit("syscmd", {
+        msg: "new",
+        gender: chatPage.gender,
+        vipCode: chatPage.vipCode,
+        age: store.get("ageNew"),
+        ageWant: store.get("ageWant"),
+        secretCode: chatPage.secretCode,
+        province: chatPage.province,
+        provinceWant: store.get("provinceWant"),
+        savedId: chatPage.usrId,
+        isShowLocation: false,
+        fp: chatPage.fingerPrints,
+        phoneNumber: store.get(chatPage.Config.STORE_PHONE_NUMBER),
+        userPwdEncrypted: store.get(chatPage.Config.STORE_PASSWORD_ENCRYPTED)
       });
     }
-    var _0xb8efba = 9;
-    let _0xb4cdaa = true;
-    _0xb8efba = "papqib".split("").reverse().join("");
-    chatPage['\u0073\u0063\u0072\u0069\u0070\u0074\u0049\u0073\u0052\u0075\u006E'] = false;
+    justonce = true;
+    chatPage.scriptIsRun = false;
     setInterval(function () {
-      if (!chatPage['\u0073\u0063\u0072\u0069\u0070\u0074\u0049\u0073\u0052\u0075\u006E']) {
-        return;
-      }
-      switch (chatPage['\u0063\u0068\u0061\u0074\u0053\u0074\u0061\u0074\u0065']) {
-        case "\u0052\u0045\u0050\u0041\u0049\u0052\u0045\u0044":
-        case "DERIAP".split("").reverse().join(""):
-          {
-            let _0xcfc = chatPage['\u0070\u0061\u0072\u0074\u006E\u0065\u0072']['\u0073\u0074\u0072\u0047\u0065\u006E\u0064\u0065\u0072'];
-            if (_0xcfc == chatPage['\u004D\u0061\u0074\u0063\u0068\u0057\u0068\u006F']) {
-              if (_0xb4cdaa) {
-                try {
-                  let _0x37cd = AUTO_MSG['\u006C\u0065\u006E\u0067\u0074\u0068'];
-                  if (_0x37cd > 1) {
-                    const _0xg97b9a = Math['\u0066\u006C\u006F\u006F\u0072'](Math['\u0072\u0061\u006E\u0064\u006F\u006D']() * _0x37cd);
-                    let selectedMessage = AUTO_MSG[_0xg97b9a];
-                    let messages = selectedMessage['\u0073\u0070\u006C\u0069\u0074']("\u007C")['\u006D\u0061\u0070'](msg => msg['\u0074\u0072\u0069\u006D']());
-                    currentAutoIndex = 0;
-                    _0x7452c(messages);
-                  } else {
-                    var _0x9921c = 9;
-                    let selectedMessage = AUTO_MSG[index];
-                    _0x9921c = 4;
-                    let messages = selectedMessage['\u0073\u0070\u006C\u0069\u0074']("\u007C")['\u006D\u0061\u0070'](msg => msg['\u0074\u0072\u0069\u006D']());
-                    currentAutoIndex = 0;
-                    _0x7452c(messages);
-                  }
-                } catch {}
-                $['\u0074\u006F\u0061\u0073\u0074']("\u5230\u914D\u5339\u5DF2".split("").reverse().join("") + chatPage['\u004D\u0061\u0074\u0063\u0068\u0057\u0068\u006F'] + "\u5E76\u53D1\u9001\u4FE1\u606F\uFF01", 800);
-                _0xb4cdaa = false;
+      {
+        if (!chatPage.scriptIsRun) {
+          return;
+        }
+        switch (chatPage.chatState) {
+          case "REPAIRED":
+          case "PAIRED":
+            {
+              var _0x563b95 = chatPage.partner.strGender;
+              if (_0x563b95 == chatPage.MatchWho) {
+                if (justonce) {
+                  try {
+                    let _0xc6c72d = AUTO_MSG.length;
+                    if (_0xc6c72d > 0) {
+                      {
+                        let _0x5842c0 = AUTO_MSG[index];
+                        _0x43a392(_0x5842c0);
+                      }
+                    }
+                  } catch {}
+                  $.toast("已匹配到" + chatPage.MatchWho + "并发送信息！", 800);
+                  justonce = false;
+                }
+                break;
               }
+              _0x233162();
+              _0x5607fd();
+              justonce = true;
               break;
             }
-            _0x777cfd();
-            _0x4c87fd();
-            _0xb4cdaa = true;
+          case "SELF_LEFT":
+          case "PARTNER_LEFT":
+            _0x5607fd();
+            justonce = true;
             break;
-          }
-        case "\u0053\u0045\u004C\u0046\u005F\u004C\u0045\u0046\u0054":
-        case "\u0050\u0041\u0052\u0054\u004E\u0045\u0052\u005F\u004C\u0045\u0046\u0054":
-          _0x4c87fd();
-          _0xb4cdaa = true;
-          break;
+        }
       }
     }, 450);
-    function _0x7452c(messages) {
-      if (currentAutoIndex < messages['\u006C\u0065\u006E\u0067\u0074\u0068']) {
-        _0x6c36d(messages[currentAutoIndex]);
-        currentAutoIndex++;
-        setTimeout(() => {
-          _0x7452c(messages);
-        }, 1500);
+    var _0x11bfb3 = $("<div class=\"list-block\"><ul><li>                        <div class=\"item-content\"><div class=\"item-inner\"><div class=\"item-title label\">脚本启动</div><div class=\"item-input\"><label class=\"label-switch\"><input tabindex=\"-1\" type=\"checkbox\" id=\"ckb_script\"><div class=\"checkbox\"></div></label></div></div></div>                        <div class=\"item-content\"><div class=\"item-inner\"><div class=\"item-title label\" id=\"txt_match\">仅女生</div><div class=\"item-input\"><label class=\"label-switch\"><input tabindex=\"-1\" type=\"checkbox\" id=\"ckb_match\"><div class=\"checkbox\"></div></label></div></div></div>                        <div class=\"item-content\"><div class=\"item-inner\"><div class=\"item-title label\">发送QQ<br></div><div style=\"width: 100%; text-align: left;\" id=\"btn_sendqq\">发送</div></div></div>                        </li></ul></div>");
+    $("#panelLeft .content-block").eq(1).before(_0x11bfb3);
+    let _0x75500f = false;
+    let _0x33308c = false;
+    function _0x5f4ff5() {
+      {
+        _0x75500f = !_0x75500f;
+        chatPage.scriptIsRun = _0x75500f;
+        _0x3c78a2(chatPage.scriptIsRun ? "脚本已启动！" : "脚本已停止！");
       }
     }
-    var _0x_0x466 = 15;
-    var _0xg5gc5a = $("<div class=\"list-block\"><ul><li>                        <div class=\"item-content\"><div class=\"item-inner\"><div class=\"item-title label\">脚本启动</div><div class=\"item-input\"><label class=\"label-switch\"><input tabindex=\"-1\" type=\"checkbox\" id=\"ckb_script\"><div class=\"checkbox\"></div></label></div></div></div>                        <div class=\"item-content\"><div class=\"item-inner\"><div class=\"item-title label\" id=\"txt_match\">仅匹女生</div><div class=\"item-input\"><label class=\"label-switch\"><input tabindex=\"-1\" type=\"checkbox\" id=\"ckb_match\"><div class=\"checkbox\"></div></label></div></div></div>                        <div class=\"item-content\"><div class=\"item-inner\"><div class=\"item-title label\">发送联系<br></div><div style=\"width: 100%; text-align: left;\" id=\"btn_sendqq\">发送</div></div></div>                        </li></ul></div>");
-    _0x_0x466 = 9;
-    $("\u0023\u0070\u0061\u006E\u0065\u006C\u004C\u0065\u0066\u0074\u0020\u002E\u0063\u006F\u006E\u0074\u0065\u006E\u0074\u002D\u0062\u006C\u006F\u0063\u006B")['\u0065\u0071'](1)['\u0062\u0065\u0066\u006F\u0072\u0065'](_0xg5gc5a);
-    let _0x023d = false;
-    let _0x75caf = false;
-    function _0xe_0x5aa() {
-      _0x023d = !_0x023d;
-      chatPage['\u0073\u0063\u0072\u0069\u0070\u0074\u0049\u0073\u0052\u0075\u006E'] = _0x023d;
-      _0xf569a(chatPage['\u0073\u0063\u0072\u0069\u0070\u0074\u0049\u0073\u0052\u0075\u006E'] ? "\uFF01\u52A8\u542F\u5DF2\u672C\u811A".split("").reverse().join("") : "\uFF01\u6B62\u505C\u5DF2\u672C\u811A".split("").reverse().join(""));
+    function _0x59c9af() {
+      _0x33308c = !_0x33308c;
+      var _0x2c6dec = _0x33308c;
+      chatPage.MatchWho = _0x2c6dec ? "女生" : "男生";
+      $("#txt_match")[0].innerText = "仅" + chatPage.MatchWho;
     }
-    function _0xfa0f() {
-      _0x75caf = !_0x75caf;
-      var _0xb_0x40a = _0x75caf;
-      chatPage['\u004D\u0061\u0074\u0063\u0068\u0057\u0068\u006F'] = _0xb_0x40a ? "\u751F\u5973".split("").reverse().join("") : "\u7537\u751F";
-      $("\u0023\u0074\u0078\u0074\u005F\u006D\u0061\u0074\u0063\u0068")[0]['\u0069\u006E\u006E\u0065\u0072\u0054\u0065\u0078\u0074'] = "\u5339\u4EC5".split("").reverse().join("") + chatPage['\u004D\u0061\u0074\u0063\u0068\u0057\u0068\u006F'];
-    }
-    function _0xda5d() {
+    function _0x38eb20() {
       try {
-        var _0xc78ede;
-        const _0xge_0x7bb = QQ['\u0074\u0072\u0069\u006D']();
-        _0xc78ede = 6;
-        if (new RegExp('\u005E\u005C\u0064\u007B\u0035\u002C\u0031\u0032\u007D\u0024', "")['\u0074\u0065\u0073\u0074'](_0xge_0x7bb)) {
-          var _0x675d4g = 10;
-          let _0xe9b5fg = _0xge_0x7bb['\u006C\u0065\u006E\u0067\u0074\u0068'];
-          _0x675d4g = 8;
-          if (_0xe9b5fg > 6 && _0xe9b5fg <= 10) {
-            setTimeout(() => _0x6c36d(_0xge_0x7bb['\u0073\u0075\u0062\u0073\u0074\u0072'](0, 3)), 1000);
-            setTimeout(() => _0x6c36d(_0xge_0x7bb['\u0073\u0075\u0062\u0073\u0074\u0072'](3, 3)), 2000);
-            if (_0xe9b5fg == 10) {
-              setTimeout(() => _0x6c36d(_0xge_0x7bb['\u0073\u0075\u0062\u0073\u0074\u0072'](6, 3)), 3000);
-              setTimeout(() => _0x6c36d(_0xge_0x7bb['\u0073\u0075\u0062\u0073\u0074\u0072'](9)), 3500);
-            } else {
-              setTimeout(() => _0x6c36d(_0xge_0x7bb['\u0073\u0075\u0062\u0073\u0074\u0072'](6)), 3000);
-            }
-          } else {
-            _0x6c36d(_0xge_0x7bb);
+        var _0x145855 = QQ.length;
+        _0x145855 > 6 && _0x145855 <= 10 && (setTimeout(function () {
+          _0x43a392(QQ.substr(0, 3));
+        }, 1000), setTimeout(function () {
+          _0x43a392(QQ.substr(3, 3));
+        }, 2000), _0x145855 == 10 ? (setTimeout(function () {
+          {
+            _0x43a392(QQ.substr(6, 3));
           }
-        } else {
-          _0x6c36d(_0xge_0x7bb);
-        }
-      } catch (error) {}
+        }, 3000), setTimeout(function () {
+          {
+            _0x43a392(QQ.substr(9));
+          }
+        }, 3500)) : setTimeout(function () {
+          {
+            _0x43a392(QQ.substr(6));
+          }
+        }, 3000));
+      } catch (_0x2bb7b2) {}
     }
-    $("\u0023\u0063\u006B\u0062\u005F\u0073\u0063\u0072\u0069\u0070\u0074")[0]['\u006F\u006E\u0063\u006C\u0069\u0063\u006B'] = _0xe_0x5aa;
-    $("\u0023\u0063\u006B\u0062\u005F\u006D\u0061\u0074\u0063\u0068")[0]['\u006F\u006E\u0063\u006C\u0069\u0063\u006B'] = _0xfa0f;
-    $("\u0023\u0062\u0074\u006E\u005F\u0073\u0065\u006E\u0064\u0071\u0071")[0]['\u006F\u006E\u0063\u006C\u0069\u0063\u006B'] = _0xda5d;
+    $("#ckb_script")[0].onclick = _0x5f4ff5;
+    $("#ckb_match")[0].onclick = _0x59c9af;
+    $("#btn_sendqq")[0].onclick = _0x38eb20;
     setTimeout(() => {
-      $("\u0023\u0069\u006E\u0074\u0072\u006F\u0054\u0069\u0074\u006C\u0065")['\u0072\u0065\u006D\u006F\u0076\u0065']();
-      $("nEeltiTortni#".split("").reverse().join(""))['\u0072\u0065\u006D\u006F\u0076\u0065']();
-      $("eltit.".split("").reverse().join(""))['\u0072\u0065\u006D\u006F\u0076\u0065']();
-      $("\u002E\u0074\u0069\u0074\u006C\u0065\u002D\u0065\u006E")['\u0072\u0065\u006D\u006F\u0076\u0065']();
-      $("\u0023\u0070\u0061\u006E\u0065\u006C\u004C\u0065\u0066\u0074\u0020\u002E\u0063\u006F\u006E\u0074\u0065\u006E\u0074\u002D\u0062\u006C\u006F\u0063\u006B")['\u0065\u0071'](1)['\u0072\u0065\u006D\u006F\u0076\u0065']();
-      $("\u0023\u0070\u0061\u006E\u0065\u006C\u004C\u0065\u0066\u0074\u0020\u002E\u0063\u006F\u006E\u0074\u0065\u006E\u0074\u002D\u0062\u006C\u006F\u0063\u006B")['\u0065\u0071'](1)['\u0072\u0065\u006D\u006F\u0076\u0065']();
-      $("\u002E\u0062\u0075\u0074\u0074\u006F\u006E\u002E\u0062\u0075\u0074\u0074\u006F\u006E\u002D\u006C\u0069\u006E\u006B\u002E\u0062\u0075\u0074\u0074\u006F\u006E\u002D\u006E\u0061\u0076\u002E\u0070\u0075\u006C\u006C\u002D\u0072\u0069\u0067\u0068\u0074")['\u0072\u0065\u006D\u006F\u0076\u0065']();
-      $("\u002E\u0062\u0075\u0074\u0074\u006F\u006E\u002E\u0062\u0075\u0074\u0074\u006F\u006E\u002D\u006C\u0069\u006E\u006B\u002E\u0062\u0075\u0074\u0074\u006F\u006E\u002D\u006E\u0061\u0076\u002E\u0070\u0075\u006C\u006C\u002D\u0072\u0069\u0067\u0068\u0074")['\u0072\u0065\u006D\u006F\u0076\u0065']();
-      $("\u0023\u0063\u006B\u0062\u005F\u006D\u0061\u0074\u0063\u0068")['\u0063\u006C\u0069\u0063\u006B']();
-      chatPage['\u004D\u0061\u0074\u0063\u0068\u0057\u0068\u006F'] = "\u751F\u5973".split("").reverse().join("");
-      _0xf569a("\u9A6D\u5E08\u0036\u0039\u53F7\u4E13\u7528\u63D2\u4EF6\uFF0C\u5982\u6709\u7591\u95EE\u8BF7\u53CA\u65F6\u53CD\u9988\u0013\u0013");
+      {
+        $("#panelLeft .content-block").eq(1).remove();
+        $("#panelLeft .content-block").eq(1).remove();
+        $(".button.button-link.button-nav.pull-right").remove();
+        $(".button.button-link.button-nav.pull-right").remove();
+        $("#ckb_match").click();
+        chatPage.MatchWho = "女生";
+        _0x3c78a2("听说内群专属，电报频道@bdsm368\r\n从倒卖者处获取插件导致无法使用我们概不负责");
+      }
     }, 3);
   }
   try {
-    initialized && console['\u006C\u006F\u0067']("\uFF01\u5316\u59CB\u521D\u5DF2".split("").reverse().join(""));
+    initialized && console.log("已初始化！");
   } catch {
     initialized = true;
-    _0xa_0x4ed();
+    _0x1a8d9a();
   }
 };
 MainFunc();
